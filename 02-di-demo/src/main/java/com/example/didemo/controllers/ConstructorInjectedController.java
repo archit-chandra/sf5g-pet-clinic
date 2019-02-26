950090c784bb6ed9770e54e79acc2e2f6cc6ab5d
archit_chandra <send2archit.chandra@gmail.com>
2019-02-27 00:54:04 +0100

    Spring DI: added necessary annotations @Controller, @Autowired, @Service where needed

--- 02-di-demo/src/main/java/com/example/didemo/controllers/ConstructorInjectedController.java
+++ 02-di-demo/src/main/java/com/example/didemo/controllers/ConstructorInjectedController.java
@@ -1,11 +1,16 @@
 package com.example.didemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import com.example.didemo.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- 02-di-demo/src/main/java/com/example/didemo/controllers/PropertyInjectedController.java
+++ 02-di-demo/src/main/java/com/example/didemo/controllers/PropertyInjectedController.java
@@ -1,9 +1,14 @@
 package com.example.didemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import com.example.didemo.services.GreetingService;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String sayHello() {

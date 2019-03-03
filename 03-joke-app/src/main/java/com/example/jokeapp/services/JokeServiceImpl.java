3f1ee71b22a4ed37f016bd7607c7fa06ce5eb458
archit_chandra <send2archit.chandra@gmail.com>
2019-03-03 06:22:33 +0100

    #31 Spring configuration java: injecting the initialized bean from config class

--- 03-joke-app/src/main/java/com/example/jokeapp/services/JokeServiceImpl.java
+++ 03-joke-app/src/main/java/com/example/jokeapp/services/JokeServiceImpl.java
@@ -9,12 +9,13 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    // ChuckNorrisQuotes bean will be initialized from config class "ChuckConfiguration" and not from the constructor
+    // It is a better way when using some externalized dependency
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
-
     public String getJoke() {
         // Very inefficient - instance will be created for each request
         //ChuckNorrisQuotes chuckNorrisQuotes=new ChuckNorrisQuotes();

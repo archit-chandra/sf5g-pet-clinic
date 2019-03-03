ef83e27ca4667d68052ec5f9baa2669be74f9b60
archit_chandra <send2archit.chandra@gmail.com>
2019-03-03 06:41:51 +0100

    #33 Reverting to Spring Java configuration (from xml)

--- 03-joke-app/src/main/java/com/example/jokeapp/JokeappApplication.java
+++ 03-joke-app/src/main/java/com/example/jokeapp/JokeappApplication.java
@@ -2,10 +2,8 @@ package com.example.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {

416261663284b98702829a860513b86f6e4a89a2
archit_chandra <send2archit.chandra@gmail.com>
2019-03-03 01:42:29 +0100

    #6 Map based services: added map based services for Owner, Pet, Vet

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.gemalto.petclinic.services.map;
+
+import java.util.Set;
+
+import com.gemalto.petclinic.models.Pet;
+import com.gemalto.petclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+}

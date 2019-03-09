38cf5912ba1e3fec534f3b980dc05e91b94d6817
archit_chandra <send2archit.chandra@gmail.com>
2019-03-09 17:17:53 +0100

    #34 demo spring factory beans: modified service layer to use DAO repository layer to get the greetings (== configurations)

--- 02-di-demo/src/main/java/com/example/didemo/services/PrimaryGreetingService.java
+++ 02-di-demo/src/main/java/com/example/didemo/services/PrimaryGreetingService.java
@@ -1,16 +1,26 @@
 package com.example.didemo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+
+import com.example.didemo.dao.GreetingRepository;
 
 @Service
 @Primary
 @Profile({"en", "default"})
 public class PrimaryGreetingService implements GreetingService {
 
+    private GreetingRepository greetingRepository;
+
+    @Autowired
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "I am injected by the PrimaryGreetingService";
+        return greetingRepository.getEnglishGreeting();
     }
 }
--- 02-di-demo/src/main/java/com/example/didemo/services/PrimarySpanishGreetingService.java
+++ 02-di-demo/src/main/java/com/example/didemo/services/PrimarySpanishGreetingService.java
@@ -1,16 +1,26 @@
 package com.example.didemo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+
+import com.example.didemo.dao.GreetingRepository;
 
 @Service
 @Profile("es")
 @Primary
 public class PrimarySpanishGreetingService implements GreetingService {
 
+    private GreetingRepository greetingRepository;
+
+    @Autowired
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hola!!";
+        return greetingRepository.getSpanishGreeting();
     }
 }

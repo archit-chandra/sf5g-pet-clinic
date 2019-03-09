7281329ac535d374a38611a4322dd2e12ab33d20
archit_chandra <send2archit.chandra@gmail.com>
2019-03-10 00:24:40 +0100

    #16 List all Vets on Vet index page: VetController now gets owner list from VetService

--- pet-clinic/pet-clinic-web/src/main/java/com/gemalto/petclinic/controllers/VetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/gemalto/petclinic/controllers/VetController.java
@@ -1,15 +1,27 @@
 package com.gemalto.petclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gemalto.petclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }

565572c416bb30f3bd997dfd7baa20a681f46a11
archit_chandra <send2archit.chandra@gmail.com>
2019-03-09 17:51:25 +0100

    #34 demo spring factory beans: service beans are defined in spring config class now (less scattering)

--- 02-di-demo/src/main/java/com/example/didemo/services/PrimaryFrenchGreetingService.java
+++ 02-di-demo/src/main/java/com/example/didemo/services/PrimaryFrenchGreetingService.java
@@ -1,20 +1,11 @@
 package com.example.didemo.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 import com.example.didemo.dao.GreetingRepository;
 
-@Service
-@Primary
-@Profile("fr")
 public class PrimaryFrenchGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
-    @Autowired
     public PrimaryFrenchGreetingService(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
     }

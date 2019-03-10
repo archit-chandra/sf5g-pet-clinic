920bf1aa52c9f72a8830f3fae8b414baf4bc9a32
archit_chandra <send2archit.chandra@gmail.com>
2019-03-10 01:39:50 +0100

    #37 demo - external properties in spring: environment properties in Spring

--- 02-di-demo/src/main/java/com/example/didemo/config/PropertyConfig.java
+++ 02-di-demo/src/main/java/com/example/didemo/config/PropertyConfig.java
@@ -1,16 +1,21 @@
 package com.example.didemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import com.example.didemo.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    private Environment env;
 
     @Value("${guru.username}")
     private String user;
@@ -24,7 +29,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        //fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;

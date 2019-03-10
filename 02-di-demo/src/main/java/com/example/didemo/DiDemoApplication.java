bdfd6be08c10f95284a3b4e227ca476082531b44
archit_chandra <send2archit.chandra@gmail.com>
2019-03-10 01:23:09 +0100

    #37 demo - external properties in spring: cleared the main class and called methods on FakeDataSource bean

--- 02-di-demo/src/main/java/com/example/didemo/DiDemoApplication.java
+++ 02-di-demo/src/main/java/com/example/didemo/DiDemoApplication.java
@@ -4,24 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.example.didemo.controllers.ConstructorInjectedController;
-import com.example.didemo.controllers.MyController;
-import com.example.didemo.controllers.PropertyInjectedController;
-import com.example.didemo.controllers.SetterInjectedController;
+import com.example.didemo.examplebeans.FakeDataSource;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
-        MyController controller = (MyController) ctx.getBean("myController");
-        controller.hello();
-        System.out.println("==========================================");
-        System.out.println(ctx.getBean(MyController.class).sayHello());
-        System.out.println("==========================================");
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println("=======>>>> User    : " + fakeDataSource.getUser());
+        System.out.println("=======>>>> Password: " + fakeDataSource.getPassword());
+        System.out.println("=======>>>> Url     : " + fakeDataSource.getUrl());
     }
 
 }

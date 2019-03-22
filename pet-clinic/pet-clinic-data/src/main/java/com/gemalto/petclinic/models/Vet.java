18151be04da543b9457d8f43287d801a18c7f677
archit_chandra <send2archit.chandra@gmail.com>
2019-03-23 00:28:18 +0100

    #41 Create Entity - Vet Specialty, Associate To Vet: associated speciality with vet

--- pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/models/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/models/Vet.java
@@ -1,8 +1,20 @@
 package com.gemalto.petclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }

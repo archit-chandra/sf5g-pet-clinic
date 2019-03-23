52eae88b8fc605d394d45830b0ef6f5d4d2bb16d
archit_chandra <send2archit.chandra@gmail.com>
2019-03-23 01:07:57 +0100

    #46 Add PetTypes in with Bootstrap (startup): added pet types at startup

--- pet-clinic/pet-clinic-web/src/main/java/com/gemalto/petclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/gemalto/petclinic/bootstrap/DataLoader.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.gemalto.petclinic.models.Owner;
+import com.gemalto.petclinic.models.PetType;
 import com.gemalto.petclinic.models.Vet;
 import com.gemalto.petclinic.services.OwnerService;
+import com.gemalto.petclinic.services.PetTypeService;
 import com.gemalto.petclinic.services.VetService;
 
 @Component
@@ -14,15 +16,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner("Michael", "Weston");
         ownerService.save(owner1);
 

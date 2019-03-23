b63b87ca711bdaf214f2d0eef8e80ef1300d417e
archit_chandra <send2archit.chandra@gmail.com>
2019-03-23 18:10:00 +0100

    #48 Create Specialties, add to Vets (startup):  ensuring that every speciality associated with vet has an id

--- pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/services/map/VetServiceMap.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/services/map/VetServiceMap.java
@@ -4,11 +4,19 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.gemalto.petclinic.models.Speciality;
 import com.gemalto.petclinic.models.Vet;
+import com.gemalto.petclinic.services.SpecialityService;
 import com.gemalto.petclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -22,6 +30,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if (!vet.getSpecialities().isEmpty()) {
+            vet.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(vet);
     }
 

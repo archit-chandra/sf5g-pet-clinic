642abf07e73f9ad386c79bedf5c5196a288c508a
archit_chandra <send2archit.chandra@gmail.com>
2019-03-23 17:16:34 +0100

    #45 Create Map Service - Specialty: created implementation

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/services/map/SpeacialityServiceMap.java
@@ -0,0 +1,37 @@
+package com.gemalto.petclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.gemalto.petclinic.models.Speciality;
+import com.gemalto.petclinic.services.SpecialityService;
+
+@Service
+public class SpeacialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}

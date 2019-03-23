841fcc667de55245e01ffed68b1dd3c85880d3ee
archit_chandra <send2archit.chandra@gmail.com>
2019-03-23 22:24:37 +0100

    #62 demo: @OneToMany JPA relationship - created one-to-many relationship between recipe and ingredient

--- 04-recipe-app/src/main/java/com/example/recipeapp/domain/Recipe.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/domain/Recipe.java
@@ -1,11 +1,14 @@
 package com.example.recipeapp.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -22,8 +25,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
     // TODO: add Difficulty
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -109,5 +116,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }

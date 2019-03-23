de0968aa78ae8a69f300c1fc46e2dbde284fa3fb
archit_chandra <send2archit.chandra@gmail.com>
2019-03-23 16:53:16 +0100

    #47 Enhance Owners with contact info and Pets (startup): initialized HashSet for pets and specialities

--- pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/models/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/models/Vet.java
@@ -1,10 +1,11 @@
 package com.gemalto.petclinic.models;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
-    private Set<Speciality> specialities;
+    private Set<Speciality> specialities = new HashSet<>();
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);

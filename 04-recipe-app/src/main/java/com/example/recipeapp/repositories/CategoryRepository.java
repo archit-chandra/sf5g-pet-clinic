078078e10b8c2b85b289acd81658d3bfb64e58d9
archit_chandra <send2archit.chandra@gmail.com>
2019-03-24 04:02:18 +0100

    #68 demo: spring data JPA query methods - created query method (findByDescription) on category and unit of measure repositories

--- 04-recipe-app/src/main/java/com/example/recipeapp/repositories/CategoryRepository.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/repositories/CategoryRepository.java
@@ -1,8 +1,12 @@
 package com.example.recipeapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.recipeapp.domain.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- 04-recipe-app/src/main/java/com/example/recipeapp/repositories/UnitOfMeasureRepository.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -1,8 +1,12 @@
 package com.example.recipeapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.recipeapp.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }

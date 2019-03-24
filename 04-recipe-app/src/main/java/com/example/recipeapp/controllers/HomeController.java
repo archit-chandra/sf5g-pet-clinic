2d1f9b6725015d23a15808c9e323b38306b2ab52
archit_chandra <send2archit.chandra@gmail.com>
2019-03-24 04:03:55 +0100

    #68 demo: spring data JPA query methods - fetching and displaying results from query methods

--- 04-recipe-app/src/main/java/com/example/recipeapp/controllers/HomeController.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/controllers/HomeController.java
@@ -1,13 +1,34 @@
 package com.example.recipeapp.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.recipeapp.domain.Category;
+import com.example.recipeapp.domain.UnitOfMeasure;
+import com.example.recipeapp.repositories.CategoryRepository;
+import com.example.recipeapp.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class HomeController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public HomeController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @GetMapping({"", "/", "index", "index.html"})
-    public String getHomePage(){
+    public String getHomePage(Model model) {
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("===================> Category Id     : " + category.get().getId());
+        System.out.println("===================> UnitOfMeasure Id: " + unitOfMeasure.get().getId());
         return "index";
     }
 }

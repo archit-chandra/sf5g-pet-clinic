e04650e6797fcc1653199a380ec69efa524f785b
archit_chandra <send2archit.chandra@gmail.com>
2019-04-07 00:41:18 +0200

    #54 Add Spring Data JPA service for Owners: added spring data service for owner

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,52 @@
+package com.gemalto.petclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.gemalto.petclinic.models.Owner;
+import com.gemalto.petclinic.repositories.OwnerRepository;
+import com.gemalto.petclinic.services.OwnerService;
+
+@Service
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}

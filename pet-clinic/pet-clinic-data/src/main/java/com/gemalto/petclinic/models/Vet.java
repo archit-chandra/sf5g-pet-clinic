53858b7db81b62ead10ea43c71bb4d605730df25
archit_chandra <send2archit.chandra@gmail.com>
2019-04-06 21:05:32 +0200

    #51 Convert Vets to JPA entities: added JPA annotations

--- pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/models/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/models/Vet.java
@@ -3,8 +3,21 @@ package com.gemalto.petclinic.models;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Vet(String firstName, String lastName) {

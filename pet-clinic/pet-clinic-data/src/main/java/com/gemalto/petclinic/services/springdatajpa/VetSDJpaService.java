1f408331a5f2c23438a038a744fd85eaabf8fe1b
archit_chandra <send2archit.chandra@gmail.com>
2019-04-07 00:54:24 +0200

    #55 Add Spring Data JPA service for Vets: added

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,49 @@
+package com.gemalto.petclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.gemalto.petclinic.models.Vet;
+import com.gemalto.petclinic.repositories.VetRepository;
+import com.gemalto.petclinic.services.VetService;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}

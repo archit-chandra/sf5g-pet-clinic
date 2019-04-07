0efa5da39e2c92d5d577ae0c5676be65685ac054
archit_chandra <send2archit.chandra@gmail.com>
2019-04-08 00:26:08 +0200

    #70 Create Visit Map service: added Visit Map service

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package com.gemalto.petclinic.services.map;
+
+import java.util.Set;
+
+import com.gemalto.petclinic.models.Visit;
+import com.gemalto.petclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        // FIXME:
+        //  1. Implement custom exceptions to know about the issue
+        //  2. Below code can be improved
+        if (visit.getPet() == null ||
+                visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null ||
+                visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}

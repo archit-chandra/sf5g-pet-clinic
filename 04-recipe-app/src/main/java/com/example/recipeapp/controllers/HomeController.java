665cce9852726b6af31dc2413ce04997e6ae41c2
archit_chandra <send2archit.chandra@gmail.com>
2019-04-21 02:24:50 +0200

    #92 demo: display recipe by id - added controller method for getRecipeById ("/recipe/show/{id}") to show recipe (detail) page ("recipe/show")

--- 04-recipe-app/src/main/java/com/example/recipeapp/controllers/HomeController.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.example.recipeapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.recipeapp.service.RecipeService;
 
@@ -23,5 +24,11 @@ public class HomeController {
         log.debug("getting index page ...");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
+    }
+
+    @GetMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+        return "recipe/show";
     }
 }

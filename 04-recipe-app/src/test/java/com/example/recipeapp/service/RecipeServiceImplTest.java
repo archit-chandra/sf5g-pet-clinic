3a5b79a91f03dc48f9a5ea2959ce4b4622496a10
archit_chandra <send2archit.chandra@gmail.com>
2019-04-21 02:17:31 +0200

    #92 demo: display recipe by id - added test for getRecipeById for recipe service

--- 04-recipe-app/src/test/java/com/example/recipeapp/service/RecipeServiceImplTest.java
+++ 04-recipe-app/src/test/java/com/example/recipeapp/service/RecipeServiceImplTest.java
@@ -1,11 +1,15 @@
 package com.example.recipeapp.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.Before;
@@ -37,10 +41,26 @@ public class RecipeServiceImplTest {
         verify(recipeRepository, times(1)).findAll();
     }
 
-    private HashSet<Recipe> getRecipesData() {
+    private Set<Recipe> getRecipesData() {
         Recipe recipe = new Recipe();
         HashSet<Recipe> recipeData = new HashSet<>();
         recipeData.add(recipe);
         return recipeData;
+    }
+
+    private Optional<Recipe> getOptionalRecipe() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        return Optional.of(recipe);
+    }
+
+    @Test
+    public void getRecipeByIdTest() {
+        when(recipeRepository.findById(anyLong())).thenReturn(getOptionalRecipe());
+        Recipe returnedRecipe = recipeService.findById(1L);
+        assertNotNull(returnedRecipe);
+        // default times() = 1, it can be omitted
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
 }

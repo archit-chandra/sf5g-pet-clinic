b230d20e80d345aa7e8500f2c8a989dc32d59780
archit_chandra <send2archit.chandra@gmail.com>
2019-04-27 21:00:04 +0200

    #95 demo: command object and type conversion - added converters to interchangeably convert between command objects and domain objects

---
+++ 04-recipe-app/src/main/java/com/example/recipeapp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -0,0 +1,28 @@
+package com.example.recipeapp.converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import com.example.recipeapp.commands.UnitOfMeasureCommand;
+import com.example.recipeapp.domain.UnitOfMeasure;
+
+import lombok.Synchronized;
+
+@Component
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+        if (unitOfMeasure != null) {
+            final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(unitOfMeasure.getId());
+            uomc.setDescription(unitOfMeasure.getDescription());
+            return uomc;
+        }
+        return null;
+    }
+}

c9a20690a76d29521c04943f069888062d8d31b9
archit_chandra <send2archit.chandra@gmail.com>
2019-04-28 01:15:58 +0200

    #94 demo: create a recipe - refactored url to "/recipe/{id}/show" from "/recipe/show/{id}"

--- 04-recipe-app/src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -3,7 +3,9 @@ package com.example.recipeapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.recipeapp.commands.RecipeCommand;
 import com.example.recipeapp.service.RecipeService;
@@ -27,13 +29,13 @@ public class RecipeController {
         return "index";
     }
 
-    @GetMapping("/recipe/show/{id}")
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
     }
 
-    @GetMapping("recipe/new")
+    @GetMapping("/recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
         return "recipe/recipeform";
--- 04-recipe-app/src/test/java/com/example/recipeapp/controllers/RecipeControllerTest.java
+++ 04-recipe-app/src/test/java/com/example/recipeapp/controllers/RecipeControllerTest.java
@@ -1,12 +1,14 @@
 package com.example.recipeapp.controllers;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -19,10 +21,12 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import com.example.recipeapp.commands.RecipeCommand;
 import com.example.recipeapp.domain.Recipe;
 import com.example.recipeapp.service.RecipeService;
 
@@ -36,10 +40,13 @@ public class RecipeControllerTest {
     @Mock
     private Model model;
 
+    private MockMvc mockMvc;
+
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         recipeController = new RecipeController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
     }
 
     /**
@@ -50,7 +57,6 @@ public class RecipeControllerTest {
      */
     @Test
     public void testMockMvc() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"));
@@ -89,10 +95,9 @@ public class RecipeControllerTest {
     }
 
     @Test
-    public void getRecipeTest() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+    public void testGetRecipe() throws Exception {
         when(recipeService.findById(anyLong())).thenReturn(getRecipe());
-        mockMvc.perform(get("/recipe/show/1"))
+        mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
                 .andExpect(model().attributeExists("recipe"));

24d4af9be842ba55a85b75fcee42a3081ad3ac77
archit_chandra <send2archit.chandra@gmail.com>
2019-04-28 06:43:07 +0200

    #107 demo: Create separate Controller for Index page - refactored recipe controller + its test

--- 04-recipe-app/src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.recipeapp.commands.RecipeCommand;
 import com.example.recipeapp.service.RecipeService;
@@ -14,40 +15,35 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequestMapping("/recipe")
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private static final String MODEL_NAME = "recipe";
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
-    @GetMapping({"", "/", "index", "index.html"})
-    public String getHomePage(Model model) {
-        log.debug("getting index page ...");
-        model.addAttribute("recipes", recipeService.getRecipes());
-        return "index";
-    }
-
-    @GetMapping("/recipe/{id}/show")
+    @GetMapping("/{id}/show")
     public String showById(@PathVariable String id, Model model) {
-        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+        model.addAttribute(MODEL_NAME, recipeService.findById(new Long(id)));
         return "recipe/show";
     }
 
-    @GetMapping("/recipe/new")
+    @GetMapping("/new")
     public String newRecipe(Model model) {
-        model.addAttribute("recipe", new RecipeCommand());
+        model.addAttribute(MODEL_NAME, new RecipeCommand());
         return "recipe/recipeform";
     }
 
-    @GetMapping("recipe/{id}/update")
+    @GetMapping("/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
-        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        model.addAttribute(MODEL_NAME, recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeform";
     }
 
-    @PostMapping("/recipe")
+    @PostMapping("")
     public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
         RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
         return "redirect:/recipe/" + savedRecipeCommand.getId() + "/show";
@@ -55,7 +51,7 @@ public class RecipeController {
 
     // Since this URL is called by a link (always sends HTTP GET request)
     // and not within a form (where HTTP request can be set), therefore not using @DeleteMapping
-    @GetMapping("/recipe/{id}/delete")
+    @GetMapping("/{id}/delete")
     public String deleteById(@PathVariable String id) {
         log.debug("Deleting id:'{}'", id);
         recipeService.deleteById(Long.valueOf(id));
--- 04-recipe-app/src/test/java/com/example/recipeapp/controllers/RecipeControllerTest.java
+++ 04-recipe-app/src/test/java/com/example/recipeapp/controllers/RecipeControllerTest.java
@@ -1,9 +1,7 @@
 package com.example.recipeapp.controllers;
 
-import static junit.framework.TestCase.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -13,18 +11,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
 import com.example.recipeapp.commands.RecipeCommand;
 import com.example.recipeapp.domain.Recipe;
@@ -32,60 +25,16 @@ import com.example.recipeapp.service.RecipeService;
 
 public class RecipeControllerTest {
 
-    private RecipeController recipeController;
-
     @Mock
     private RecipeService recipeService;
-
-    @Mock
-    private Model model;
 
     private MockMvc mockMvc;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        recipeController = new RecipeController(recipeService);
+        RecipeController recipeController = new RecipeController(recipeService);
         mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
-    }
-
-    /**
-     * To test controllers
-     * - check status
-     * - check view name
-     * - etc
-     */
-    @Test
-    public void testMockMvc() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));
-    }
-
-    @Test
-    public void getHomePage() {
-        when(recipeService.getRecipes()).thenReturn(getRecipeData());
-        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
-
-        String viewName = recipeController.getHomePage(model);
-        assertEquals("index", viewName);
-        verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
-
-        Set<Recipe> setInController = argumentCaptor.getValue();
-        assertEquals(2, setInController.size());
-    }
-
-    private Set<Recipe> getRecipeData() {
-        Recipe recipe1 = new Recipe();
-        recipe1.setId(1L);
-        Recipe recipe2 = new Recipe();
-        recipe1.setId(2L);
-
-        Set<Recipe> recipes = new HashSet<>();
-        recipes.add(recipe1);
-        recipes.add(recipe2);
-        return recipes;
     }
 
     private Recipe getRecipe() {

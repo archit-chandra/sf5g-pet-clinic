3d396409e92ae8f5e65887b0faba59ae8da0e1ba
archit_chandra <send2archit.chandra@gmail.com>
2019-05-03 03:19:34 +0200

    #99 demo: show ingredient - added controller method to show recipe's ingredient

--- 04-recipe-app/src/main/java/com/example/recipeapp/controllers/IngredientController.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/controllers/IngredientController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.recipeapp.service.IngredientService;
 import com.example.recipeapp.service.RecipeService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 public class IngredientController {
 
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/recipe/{recipeId}/ingredients")
@@ -25,5 +28,14 @@ public class IngredientController {
         // use command object to avoid lazy load errors in Thymeleaf.
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping("/recipe/{recipeId}/ingredient/{id}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId,
+                                       @PathVariable String id,
+                                       Model model) {
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId),
+                                                                                         Long.valueOf(id)));
+        return "recipe/ingredient/show";
     }
 }

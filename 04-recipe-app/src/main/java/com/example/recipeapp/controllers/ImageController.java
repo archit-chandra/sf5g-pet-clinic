4b3a781bd3d67cb6355b02c148ca84ac84d4bb12
archit_chandra <send2archit.chandra@gmail.com>
2019-05-05 01:42:17 +0200

    #104 demo: upload images with spring mvc - added controller method (+test) to show recipe image form for a recipe

---
+++ 04-recipe-app/src/main/java/com/example/recipeapp/controllers/ImageController.java
@@ -0,0 +1,28 @@
+package com.example.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.recipeapp.service.ImageService;
+import com.example.recipeapp.service.RecipeService;
+
+@Controller
+public class ImageController {
+
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    public ImageController(ImageService imageService, RecipeService recipeService) {
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("recipe/{id}/image")
+    public String showUploadForm(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/imageuploadform";
+    }
+}
---
+++ 04-recipe-app/src/test/java/com/example/recipeapp/converters/ImageControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.recipeapp.converters;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.example.recipeapp.commands.RecipeCommand;
+import com.example.recipeapp.controllers.ImageController;
+import com.example.recipeapp.service.ImageService;
+import com.example.recipeapp.service.RecipeService;
+
+public class ImageControllerTest {
+
+    @Mock
+    ImageService imageService;
+
+    @Mock
+    RecipeService recipeService;
+
+    private ImageController imageController;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageController = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+    }
+
+    @Test
+    public void getImageForm() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(get("/recipe/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+
+    }
+}

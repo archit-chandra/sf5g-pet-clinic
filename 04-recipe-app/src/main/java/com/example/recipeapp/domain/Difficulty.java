0c1c4e90fbcd26f3aa12072aa35bda1668ff0685
archit_chandra <send2archit.chandra@gmail.com>
2019-05-04 04:40:48 +0200

    #103 demo: enumeration dropdowns in spring mvc - added TODOs for future

--- 04-recipe-app/src/main/java/com/example/recipeapp/domain/Difficulty.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/domain/Difficulty.java
@@ -1,6 +1,9 @@
 package com.example.recipeapp.domain;
 
 public enum Difficulty {
+    // TODO:
+    //  1. provide proper name to be shown on the dropdown
+    //  2. Experiment in recipeform. th:value="${difficultyValue.name()}" => th:value="${difficultyValue}"
 
     EASY,
     MODERATE,

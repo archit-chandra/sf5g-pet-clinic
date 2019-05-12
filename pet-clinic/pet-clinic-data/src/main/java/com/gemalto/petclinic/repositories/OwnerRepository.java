2b2803f2b6cb047567175ebe574c8d6075b9c1dc
archit_chandra <send2archit.chandra@gmail.com>
2019-05-12 06:01:30 +0200

    #78 Implement find Owner : added findAllByLastNameLike method on OwnerRepository

--- pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/repositories/OwnerRepository.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/gemalto/petclinic/repositories/OwnerRepository.java
@@ -1,5 +1,7 @@
 package com.gemalto.petclinic.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.gemalto.petclinic.models.Owner;
@@ -7,4 +9,6 @@ import com.gemalto.petclinic.models.Owner;
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }

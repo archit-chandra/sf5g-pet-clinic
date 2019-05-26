2e5ea3242605daa9eb81849951b72d542afb8c5d
archit_chandra <send2archit.chandra@gmail.com>
2019-05-26 22:59:44 +0200

    #127 demo: spring mvc exception handler - added exception (404) handler method

--- 04-recipe-app/src/main/java/com/example/recipeapp/controllers/RecipeController.java
+++ 04-recipe-app/src/main/java/com/example/recipeapp/controllers/RecipeController.java
@@ -1,14 +1,19 @@
 package com.example.recipeapp.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.recipeapp.commands.RecipeCommand;
+import com.example.recipeapp.exceptions.NotFoundException;
 import com.example.recipeapp.service.RecipeService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -56,5 +61,22 @@ public class RecipeController {
         log.debug("Deleting id:'{}'", id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    // NOTES:
+    //  1. handler method take precedence than exception class
+    //  2. Therefore, defined @ResponseStatus here, otherwise it will return 200 status
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
     }
 }

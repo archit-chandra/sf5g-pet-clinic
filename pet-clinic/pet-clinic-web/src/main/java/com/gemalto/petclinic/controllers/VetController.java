278dff7fb317720ca168810a88e9b6f5bc7041af
archit_chandra <send2archit.chandra@gmail.com>
2019-06-02 21:50:01 +0200

    #114 Implement Vets JSON Endpoint : added method to return JSON response

--- pet-clinic/pet-clinic-web/src/main/java/com/gemalto/petclinic/controllers/VetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/gemalto/petclinic/controllers/VetController.java
@@ -1,11 +1,15 @@
 package com.gemalto.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.gemalto.petclinic.models.Vet;
 import com.gemalto.petclinic.services.VetService;
 
 @Controller
@@ -23,5 +27,15 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    // TODO: Future Requirement (D2)
+    //  1. This service will be moved to other module with extra security implementation
+    //  2. It will filter vets (not retunn all)
+    //  3. Filtering criteria:  available date, pet type, speciality, etc. (TBD)
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }

4e5e6e1f74616e8176caf77c5388cfd4476b21ff
archit_chandra <send2archit.chandra@gmail.com>
2019-06-29 22:22:08 +0200

    #144 core-advance: spring data jpa - added repo service for Product

---
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/services/reposervices/ProductServiceRepoImpl.java
@@ -0,0 +1,46 @@
+package com.example.springcoreadvance.services.reposervices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.example.springcoreadvance.domain.Product;
+import com.example.springcoreadvance.repositories.ProductRepository;
+import com.example.springcoreadvance.services.ProductService;
+
+@Service
+@Profile({"springdatajpa", "jpadao"})
+public class ProductServiceRepoImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<?> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
+
+    @Override
+    public Product getById(Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product domainObject) {
+        return productRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.deleteById(id);
+    }
+}

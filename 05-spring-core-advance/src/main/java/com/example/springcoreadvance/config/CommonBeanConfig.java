3bafb2c7f4a3755af8efb60cd7a7ac41fdf2bd6e
archit_chandra <send2archit.chandra@gmail.com>
2019-06-29 22:20:05 +0200

    #144 core-advance: spring data jpa - added @EnableJpaRepositories to let Spring find the repositories package

--- 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/CommonBeanConfig.java
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/CommonBeanConfig.java
@@ -3,8 +3,10 @@ package com.example.springcoreadvance.config;
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories("com.example.springcoreadvance.repositories")
 public class CommonBeanConfig {
 
     @Bean

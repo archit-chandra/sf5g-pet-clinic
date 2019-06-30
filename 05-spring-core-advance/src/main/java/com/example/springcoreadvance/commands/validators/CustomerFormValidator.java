c9ec6f04d2738e47ff119563024c48a960069214
archit_chandra <send2archit.chandra@gmail.com>
2019-06-30 17:06:10 +0200

    #146 core-advance: command objects and validation - custom form validator to match the passwords(old feature of Spring)

---
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/commands/validators/CustomerFormValidator.java
@@ -0,0 +1,27 @@
+package com.example.springcoreadvance.commands.validators;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.example.springcoreadvance.commands.CustomerForm;
+
+@Component
+public class CustomerFormValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return CustomerForm.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        CustomerForm customerForm = (CustomerForm) target;
+
+        if (!customerForm.getPasswordText().equals(customerForm.getPasswordTextConf())) {
+            errors.rejectValue("passwordText", "PasswordsDontMatch.customerForm.passwordText", "Passwords Don't Match");
+            errors.rejectValue("passwordTextConf", "PasswordsDontMatch.customerForm.passwordTextConf", "Passwords Don't Match");
+        }
+    }
+}

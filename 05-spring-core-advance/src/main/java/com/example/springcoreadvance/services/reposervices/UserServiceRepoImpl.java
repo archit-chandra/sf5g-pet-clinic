80a030efa7d3d4fb6fc0cf0f188f60f2c2989c11
archit_chandra <send2archit.chandra@gmail.com>
2019-07-02 01:05:10 +0200

    #146 core-advance: command objects and validation - fixing relationship between customer, user when deleting user

--- 05-spring-core-advance/src/main/java/com/example/springcoreadvance/services/reposervices/UserServiceRepoImpl.java
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/services/reposervices/UserServiceRepoImpl.java
@@ -6,8 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.springcoreadvance.domain.User;
+import com.example.springcoreadvance.repositories.CustomerRepository;
 import com.example.springcoreadvance.repositories.UserRepository;
 import com.example.springcoreadvance.services.UserService;
 
@@ -16,10 +18,16 @@ import com.example.springcoreadvance.services.UserService;
 public class UserServiceRepoImpl implements UserService {
 
     private UserRepository userRepository;
+    private CustomerRepository customerRepository;
 
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
     @Override
@@ -40,8 +48,16 @@ public class UserServiceRepoImpl implements UserService {
     }
 
     @Override
+    @Transactional
+    // NOTES: @Transactional
+    //  1. If not used, then each repository calls will be handled in different transactions
+    //  2. Now, both below calls to userRepository, customerRepository will be in single transaction
     public void delete(Integer id) {
-        userRepository.deleteById(id);
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            customerRepository.delete(user.getCustomer());
+            userRepository.delete(user);
+        }
     }
 
     @Override

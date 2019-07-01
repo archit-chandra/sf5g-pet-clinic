1da760eb2dbfbb1f81bfba8d16f900cbf2d68fec
archit_chandra <send2archit.chandra@gmail.com>
2019-07-02 00:51:37 +0200

    #147 core-advance: spring security - (User Details Service implementation) to load user by username

---
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/services/security/SpringSecUserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.springcoreadvance.services.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.springcoreadvance.domain.User;
+import com.example.springcoreadvance.services.UserService;
+
+@Service("userDetailsService")
+public class SpringSecUserDetailsServiceImpl implements UserDetailsService {
+
+    private UserService userService;
+    private Converter<User, UserDetails> userUserDetailsConverter;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    @Qualifier(value = "userToUserDetails")
+    public void setUserUserDetailsConverter(Converter<User, UserDetails> userUserDetailsConverter) {
+        this.userUserDetailsConverter = userUserDetailsConverter;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userUserDetailsConverter.convert(userService.findByUserName(username));
+    }
+}

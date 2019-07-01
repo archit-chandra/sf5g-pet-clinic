afbc33fe8c880aa2d95166937c77e47d7b785837
archit_chandra <send2archit.chandra@gmail.com>
2019-07-02 00:20:10 +0200

    #147 core-advance: spring security - using spring security password encoder (one way encryption)

--- 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/SpringSecConfig.java
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/SpringSecConfig.java
@@ -1,11 +1,22 @@
 package com.example.springcoreadvance.config;
 
+import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+
+    // injects CommonBeanConfig >> StrongPasswordEncryptor
+    @Bean
+    public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor) {
+        PasswordEncoder passwordEncoder = new PasswordEncoder();
+        passwordEncoder.setPasswordEncryptor(passwordEncryptor);
+        return passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

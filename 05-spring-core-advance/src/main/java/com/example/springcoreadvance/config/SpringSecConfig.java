a2bc483749761a55e15e0bd5d87b9ff6f428e22e
archit_chandra <send2archit.chandra@gmail.com>
2019-07-05 01:30:20 +0200

    #147 core-advance: spring security - (DAO Authentication provider) injecting daoAuthenticationProvider to set as AuthenticationProvider

--- 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/SpringSecConfig.java
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/SpringSecConfig.java
@@ -1,14 +1,27 @@
 package com.example.springcoreadvance.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    @Qualifier("daoAuthenticationProvider")
+    public void setAuthenticationProvider(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

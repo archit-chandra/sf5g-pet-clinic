2ddecc027c0736db732bed3b85e3692de4879670
archit_chandra <send2archit.chandra@gmail.com>
2019-07-05 01:21:05 +0200

    #148 core-advance: spring security - (PasswordEncoder deprecated) BCryptPasswordEncoder used which implements org.springframework.security.crypto.password.PasswordEncoder

--- 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/SpringSecConfig.java
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/config/SpringSecConfig.java
@@ -1,21 +1,18 @@
 package com.example.springcoreadvance.config;
 
-import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
-import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecConfig extends WebSecurityConfigurerAdapter {
 
-    // injects CommonBeanConfig >> StrongPasswordEncryptor
     @Bean
-    public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor) {
-        PasswordEncoder passwordEncoder = new PasswordEncoder();
-        passwordEncoder.setPasswordEncryptor(passwordEncryptor);
-        return passwordEncoder;
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override

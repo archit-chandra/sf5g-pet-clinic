823c45757cd6d5ce4a07509ad7afc03752c33b03
archit_chandra <send2archit.chandra@gmail.com>
2019-07-05 19:45:17 +0200

    #151 core-advance: spring events - publishing an event on login failure

--- 05-spring-core-advance/src/main/java/com/example/springcoreadvance/services/security/LoginAspect.java
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/services/security/LoginAspect.java
@@ -5,12 +5,20 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoginAspect {
+
+    private LoginFailureEventPublisher publisher;
+
+    @Autowired
+    public void setPublisher(LoginFailureEventPublisher publisher) {
+        this.publisher = publisher;
+    }
 
     @Pointcut("execution(* org.springframework.security.authentication.AuthenticationProvider.authenticate(..))")
     public void doAuthenticate() {
@@ -33,5 +41,7 @@ public class LoginAspect {
     public void logAuthenicationException(Authentication authentication) {
         String userDetails = (String) authentication.getPrincipal();
         System.out.println("Login failed for user: " + userDetails);
+
+        publisher.publish(new LoginFailureEvent(authentication));
     }
 }

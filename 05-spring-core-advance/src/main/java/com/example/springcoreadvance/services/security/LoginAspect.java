4c0505e4a402e0e5106a360342489c18f3faa6f9
archit_chandra <send2archit.chandra@gmail.com>
2019-07-05 19:22:10 +0200

    #150 core-advance: AOP - added login aspect

---
+++ 05-spring-core-advance/src/main/java/com/example/springcoreadvance/services/security/LoginAspect.java
@@ -0,0 +1,37 @@
+package com.example.springcoreadvance.services.security;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoginAspect {
+
+    @Pointcut("execution(* org.springframework.security.authentication.AuthenticationProvider.authenticate(..))")
+    public void doAuthenticate() {
+
+    }
+
+    @Before("com.example.springcoreadvance.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logBefore(Authentication authentication) {
+
+        System.out.println("This is before the Authenticate Method: authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterReturning(value = "com.example.springcoreadvance.services.security.LoginAspect.doAuthenticate()",
+            returning = "authentication")
+    public void logAfterAuthenticate(Authentication authentication) {
+        System.out.println("This is after the Authenticate Method authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterThrowing("com.example.springcoreadvance.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logAuthenicationException(Authentication authentication) {
+        String userDetails = (String) authentication.getPrincipal();
+        System.out.println("Login failed for user: " + userDetails);
+    }
+}
